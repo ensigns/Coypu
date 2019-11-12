727fa79f731f47d6841cfdec3c78d909fee83414
Ryan Birmingham <birm@rbirm.us>
2019-11-12 14:24:21 -0500

    plugin manifest, basic

--- coypu.go
+++ coypu.go
@@ -2,30 +2,51 @@ package main
 
 import "github.com/fatih/color"
 import "plugin";
+import "gopkg.in/yaml.v2";
+import "io/ioutil";
 
 func main() {
 
     color.Green("[STARTUP] Reading Config")
 
     color.Green("[STARTUP] Fetching Plugins")
-    p, err := plugin.Open("./config/plugins/sampleData.so")
+    type PluginConf struct {
+        Plugins map[string] struct{
+            Path string
+            Config map[string]interface{}
+          }
+    }
+    var plugin_conf PluginConf
+    pluginConfRaw, err := ioutil.ReadFile("./config/plugins.yaml")
     if err != nil {
-      panic(err)
+        panic(err)
     }
-    //mod, err = plugin.open("config/plugins/filter.so")
-    //out, err = plugin.open("config/plugins/json.so")
-    //auth, err = plugin.open("config/plugins/json.so")
-    pl, err := p.Lookup("New")
+    err = yaml.Unmarshal(pluginConfRaw, &plugin_conf)
     if err != nil {
-    	panic(err)
+        panic(err)
     }
-    var context = map[string]interface{}{}
-    x, ok := pl.(func(map[string]interface{}) func(map[string]interface{}) map[string]interface{})
-    if !ok {
-      panic("OH NO")
+    Plugins := make(map[string]func(map[string]interface{})map[string]interface{},0)
+    for k, v := range plugin_conf.Plugins {
+      color.Yellow("[Plugin] " + k + " at " + v.Path)
+      p, err := plugin.Open(v.Path)
+      if err != nil {
+        panic(err)
+      }
+      pl, err := p.Lookup("New")
+      if err != nil {
+        panic(err)
+      }
+      x, ok := pl.(func(map[string]interface{}) func(map[string]interface{}) map[string]interface{})
+      if !ok {
+        panic("OH NO")
+      }
+      Plugins[k] = x(v.Config)
     }
-    x(context)(context)
-    color.Green("[STARTUP] Starting Server")
+    // TODO routes
 
+    color.Green("[STARTUP] Starting Server")
+    // function to generate initial context
+    // pass to list in this route
+    // function to return output from context.
     color.Green("[STARTUP] Up at")
 }
---
+++ get_deps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+go get gopkg.in/yaml.v2
+go get "github.com/fatih/color"
