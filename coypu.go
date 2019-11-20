be0d437e47a35504125d594d075e6b874238399c
Ryan Birmingham <birm@rbirm.us>
2019-11-20 12:54:35 -0500

    minor refactor

--- coypu.go
+++ coypu.go
@@ -11,6 +11,20 @@ import "os"
 import "time"
 import "strings"
 
+type PluginConf struct {
+    Plugins map[string] struct{
+        Path string
+        Config map[string]interface{}
+      }
+}
+type RouteConf struct {
+    Routes map[string] struct{
+        Path string
+        Config map[string]interface{}
+        Plugins []string
+      }
+}
+
 func getEnv(key, fallback string) string {
     value, exists := os.LookupEnv(key)
     if !exists {
@@ -19,57 +33,55 @@ func getEnv(key, fallback string) string {
     return value
 }
 
+func getPlugins() map[string]func(map[string]interface{})map[string]interface{} {
+  var plugin_conf PluginConf
+  pluginConfRaw, err := ioutil.ReadFile("./config/plugins.yaml")
+  if err != nil {
+      panic(err)
+  }
+  err = yaml.Unmarshal(pluginConfRaw, &plugin_conf)
+  if err != nil {
+      panic(err)
+  }
+  Plugins := make(map[string]func(map[string]interface{})map[string]interface{},0)
+  for k, v := range plugin_conf.Plugins {
+    color.Yellow("[Plugin] " + k + " at " + v.Path)
+    p, err := plugin.Open(v.Path)
+    if err != nil {
+      panic(err)
+    }
+    pl, err := p.Lookup("New")
+    if err != nil {
+      panic(err)
+    }
+    x, ok := pl.(func(map[string]interface{}) func(map[string]interface{}) map[string]interface{})
+    if !ok {
+      panic("OH NO")
+    }
+    Plugins[k] = x(v.Config)
+  }
+  return Plugins
+}
+
+func getRoutes()  map[string]func(map[string]interface{})map[string]interface{}{
+  var route_conf RouteConf
+  routeConfRaw, err := ioutil.ReadFile("./config/routes.yaml")
+  if err != nil {
+      panic(err)
+  }
+  err = yaml.Unmarshal(routeConfRaw, &route_conf)
+  if err != nil {
+      panic(err)
+  }
+  Routes := make(map[string]func(map[string]interface{})map[string]interface{},0)
+  return Routes
+}
+
 func main() {
     color.Green("[STARTUP] Setting Up Plugins")
-    type PluginConf struct {
-        Plugins map[string] struct{
-            Path string
-            Config map[string]interface{}
-          }
-    }
-    type RouteConf struct {
-        Routes map[string] struct{
-            Path string
-            Config map[string]interface{}
-            Plugins []string
-          }
-    }
-    var plugin_conf PluginConf
-    pluginConfRaw, err := ioutil.ReadFile("./config/plugins.yaml")
-    if err != nil {
-        panic(err)
-    }
-    err = yaml.Unmarshal(pluginConfRaw, &plugin_conf)
-    if err != nil {
-        panic(err)
-    }
-    Plugins := make(map[string]func(map[string]interface{})map[string]interface{},0)
-    for k, v := range plugin_conf.Plugins {
-      color.Yellow("[Plugin] " + k + " at " + v.Path)
-      p, err := plugin.Open(v.Path)
-      if err != nil {
-        panic(err)
-      }
-      pl, err := p.Lookup("New")
-      if err != nil {
-        panic(err)
-      }
-      x, ok := pl.(func(map[string]interface{}) func(map[string]interface{}) map[string]interface{})
-      if !ok {
-        panic("OH NO")
-      }
-      Plugins[k] = x(v.Config)
-    }
+    var Plugins = getPlugins()
     color.Green("[STARTUP] Setting Up Routes")
-    var route_conf RouteConf
-    routeConfRaw, err := ioutil.ReadFile("./config/routes.yaml")
-    if err != nil {
-        panic(err)
-    }
-    err = yaml.Unmarshal(routeConfRaw, &route_conf)
-    if err != nil {
-        panic(err)
-    }
+    var Routes = getRoutes()
     // TODO routes, using route_conf variable
     // check if the url prefix matches
     // function to generate initial context
