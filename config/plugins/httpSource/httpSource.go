45944dee3457b9f141d33abdf294ac1b25f2dec1
Ryan Birmingham <birm@rbirm.us>
2019-12-03 11:58:31 -0500

    add http and raw plugins

---
+++ config/plugins/httpSource/httpSource.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/fatih/color"
+import "io/ioutil"
+import "net/http"
+
+func New(config map[string]interface{}) func(context map[string]interface{}) map[string]interface{} {
+  return func(context map[string]interface{}) map[string]interface{} {
+    color.Red("[PKG] httpSource")
+    // get content from whatever is set as httpUrl
+    var client http.Client
+    resp, err := client.Get(context[config["urlFieldName"].(string)].(string))
+    if err != nil {
+      context["httpStatus"] = 500
+      context["httpRes"] = string(err.Error())
+    } else {
+      body, err := ioutil.ReadAll(resp.Body)
+      if err != nil {
+        context["httpStatus"] = 500
+        context["httpRes"] = string(err.Error())
+      }
+      context["httpStatus"] = 200
+      context["httpRes"] = string(body)
+    }
+    return context
+  }
+}
--- config/plugins/rawRender/rawRender.go
+++ config/plugins/rawRender/rawRender.go
@@ -4,11 +4,10 @@ import "github.com/fatih/color"
 
 func New(config map[string]interface{}) func(context map[string]interface{}) map[string]interface{} {
   return func(context map[string]interface{}) map[string]interface{} {
-    color.Red("[PKG] jsonRender")
+    color.Red("[PKG] rawRender")
     var renderFrom string = context["renderFrom"].(string)
     var renderData string = context[renderFrom].(string)
-    context["resBody"] = "{\"data\" :" + renderData + "}"
-    context["resHeaders"].(map[string]string)["Content-Type"] = "application/json"
+    context["resBody"] = renderData
     return context
   }
 }
