054995fcc1840cc673977298f0f830f4007d061a
Ryan Birmingham <birm@rbirm.us>
2019-12-04 11:14:10 -0500

    handle errors in render

--- config/plugins/jsonRender/jsonRender.go
+++ config/plugins/jsonRender/jsonRender.go
@@ -7,8 +7,13 @@ func New(config map[string]interface{}) func(context map[string]interface{}) map
     color.Red("[PKG] jsonRender")
     var renderFrom string = context["renderFrom"].(string)
     var renderData string = context[renderFrom].(string)
-    context["resBody"] = "{\"data\" :" + renderData + "}"
+
     context["resHeaders"].(map[string]string)["Content-Type"] = "application/json"
+    if context["error"] != nil {
+      context["resBody"] = "{\"data\" :\"" + renderData + "}"
+    } else {
+      context["resBody"] = "{\"err\" :\"" + context["error"].(string) + "\"}"
+    }
     return context
   }
 }
